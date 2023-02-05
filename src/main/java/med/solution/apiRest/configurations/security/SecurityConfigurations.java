7ba5f80cc2b5762914226e6735a083539cbcde10
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-02-05 14:07:01 -0300

    Spring Security - Desabilitando Bloqueio Padrão e Configurando Autenticação Stateless

---
+++ src/main/java/med/solution/apiRest/configurations/security/SecurityConfigurations.java
@@ -0,0 +1,19 @@
+package med.solution.apiRest.configurations.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity //personalizar as configurações de segurança
+public class SecurityConfigurations {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().build()
+    }
+}
