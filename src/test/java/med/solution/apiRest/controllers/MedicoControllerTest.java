0e1ef44644dc5467ee1cad02ca85516b33ecdb29
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-03-04 19:56:16 -0300

    Testes automatizados - testando codigo http400 em MedicoController

---
+++ src/test/java/med/solution/apiRest/controllers/MedicoControllerTest.java
@@ -0,0 +1,32 @@
+package med.solution.apiRest.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MedicoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Deveria retornar codigo http 400 quando informaçoes estao invalidas")
+    @WithMockUser
+    void cadastrarMedico_cenario1() throws Exception {
+        var response = mockMvc.perform(post("/medicos"))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+}
