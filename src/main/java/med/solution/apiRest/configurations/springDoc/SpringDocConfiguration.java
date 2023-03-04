67ec0c430e3559f963d53d107e7abe66873f1066
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-03-03 21:16:30 -0300

    Personalizando a Documentação - OpenAPI

--- src/main/java/med/solution/apiRest/configurations/springDoc/SpringDocConfiguration.java
+++ src/main/java/med/solution/apiRest/configurations/springDoc/SpringDocConfiguration.java
@@ -2,6 +2,9 @@ package med.solution.apiRest.configurations.springDoc;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +16,19 @@ public class SpringDocConfiguration {
         return new OpenAPI()
                 .components(new Components()
                         .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("MedSolution API")
+                        .description("API Rest da aplicação MedSolution, contendo as funcionalidades de CRUD de médicos e de pacientes, além de agendamento e cancelamento de consultas")
+                        .contact(new Contact()
+                                .name("Rodrigo Fernandes")
+                                .email("rodrigohf79@hotmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://voll.med/api/licenca")));
     }
 
 
