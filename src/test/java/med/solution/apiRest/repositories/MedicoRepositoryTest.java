e6d2330b2c97ad9853a7d6a5c745922fbb6757c0
Rodrigo Fernandes <83513696+RodrigoFernandes79@users.noreply.github.com>
2023-03-04 08:13:14 -0300

    Testes Automatizados - Configurando o banco de testes

---
+++ src/test/java/med/solution/apiRest/repositories/MedicoRepositoryTest.java
@@ -0,0 +1,18 @@
+package med.solution.apiRest.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+class MedicoRepositoryTest {
+
+    @Test
+    void escolherMedicoAleatorioNaLivreData() {
+    }
+}
